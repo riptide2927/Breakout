8f1ab9f8c971cbc0979deddc5ecaf5fe1c03e1ba
hay <moony.kartha@gmail.com>
2022-08-05 17:30:54 +0530

    project setup with premake

---
+++ Dependencies.lua
@@ -0,0 +1,12 @@
+-- Breakout dependencies
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/Vendor/GLFW/include/"
+IncludeDir["SOIL2"] = "%{wks.location}/Vendor/SOIL2/include/"
+IncludeDir["GLM"] = "%{wks.location}/Vendor/GLM/"
+IncludeDir["GLEW"] = "%{wks.location}/Vendor/GLEW/include/"
+
+LibraryDir = {}
+LibraryDir["GLFW"] = "%{wks.location}/Vendor/GLFW/lib/"
+LibraryDir["SOIL2"] = "%{wks.location}/Vendor/SOIL2/lib/"
+LibraryDir["GLEW"] = "%{wks.location}/Vendor/GLEW/lib/"
---
+++ premake5.lua
@@ -0,0 +1,64 @@
+include "Dependencies.lua"
+
+workspace "Breakout"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.architecture}-"
+
+project "Breakout"
+	location "Breakout"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("bin/" ..outputdir.. "%{prj.name}")
+	objdir("bin-int/" ..outputdir.. "%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"src",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.GLEW}",
+		"%{IncludeDir.SOIL2}",
+		"%{IncludeDir.GLM}"
+	}
+
+	libdirs
+	{
+		"%{LibraryDir.GLFW}",
+		"%{LibraryDir.GLEW}",
+		"%{LibraryDir.SOIL2}"
+	}
+
+	links
+	{
+		"soil2-debug",
+		"glfw3",
+		"glew32s",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
