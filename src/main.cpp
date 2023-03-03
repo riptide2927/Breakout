2d5dd0d2fde3e3da3657c63e46f38e7807d76913
hay <moony.kartha@gmail.com>
2023-03-03 13:43:29 +0530

    Updated build system

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 int main()
 {
-    std::cout << "Hello\n";
-    return 0;
+    if(!glfwInit())
+        std::cout << "Error initalizing glfw\n";
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    glClearColor(0.5f, 0.25f, 0.2f, 1.0f);
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    stbi_set_flip_vertically_on_load(true);
 }
